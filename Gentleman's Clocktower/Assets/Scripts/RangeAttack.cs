11c5b3875214bdc1a49363f2204924103f6164fd
BrittDibble <dibbl001@cougars.csusm.edu>
2016-10-22 14:41:43 -0700

    Start to Ranged Attack

---
+++ Gentleman's Clocktower/Assets/Scripts/RangeAttack.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RangeAttack : MonoBehaviour {
+
+	public GameObject bullet;
+	public float disappearDelay;
+	private Transform gun;
+	private float shownTime;
+
+	// Use this for initialization
+	void Start () {
+		gun = GetComponentInChildren<Transform>();
+		gun.transform.localScale = new Vector3 (0f,0f,0f);
+		shownTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown (1)){//right click
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			gun.transform.LookAt (mousePos);
+			gun.transform.rotation.x = 0f;
+			gun.transform.rotation.y = 0f;
+			gun.transform.localScale = new Vector3 (1f,1f,1f);
+			shownTime = Time.time;
+		}
+		if(Time.time - shownTime > disappearDelay)
+			gun.transform.localScale = new Vector3 (0f,0f,0f);
+	}
+}
