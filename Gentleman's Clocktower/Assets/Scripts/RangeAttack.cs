b40714bff3ca0bc325ca1a9f56c6375ed89f7391
BrittDibble <dibbl001@cougars.csusm.edu>
2016-10-22 16:44:59 -0700

    Gun points towards mouse click

--- Gentleman's Clocktower/Assets/Scripts/RangeAttack.cs
+++ Gentleman's Clocktower/Assets/Scripts/RangeAttack.cs
@@ -5,6 +5,7 @@ public class RangeAttack : MonoBehaviour {
 
 	public GameObject bullet;
 	public float disappearDelay;
+	public float zValue;
 	private Transform gun;
 	private float shownTime;
 
@@ -18,10 +19,15 @@ public class RangeAttack : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetMouseButtonDown (1)){//right click
-			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			gun.transform.LookAt (mousePos);
 			gun.transform.localScale = new Vector3 (1f,1f,1f);
 			shownTime = Time.time;
+			Vector3 mousePos = Input.mousePosition;
+			mousePos.z = Camera.main.transform.position.z * -1;//try -1 here if it doesn't work
+			Vector3 rotationObject = Camera.main.WorldToScreenPoint(gun.position);
+			mousePos.x = mousePos.x - rotationObject.x;
+			mousePos.y = mousePos.y - rotationObject.y;
+			float angle = Mathf.Atan2 (mousePos.y, mousePos.x) * Mathf.Rad2Deg - 90f;
+			gun.rotation = Quaternion.Euler (new Vector3 (0f, 0f, angle));
 		}
 		if(Time.time - shownTime > disappearDelay)
 			gun.transform.localScale = new Vector3 (0f,0f,0f);
