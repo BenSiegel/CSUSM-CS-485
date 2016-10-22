9f4ebd3026ffeed394c87e5b7e528a1ab5210014
BrittDibble <dibbl001@cougars.csusm.edu>
2016-10-22 01:16:20 -0700

    trying to fix merge conflicts

--- Gentleman's Clocktower/Assets/Scripts/PlayerController.cs
+++ Gentleman's Clocktower/Assets/Scripts/PlayerController.cs
@@ -4,34 +4,40 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
-    public float jump;
-    
+	public float speed;
+	public float jump;
+
 	private Transform tm;
 
-    bool ground = true;
 
-    void Start()
-    {
+	bool ground = true;
+
+	void Start()
+	{
 		tm = GetComponent<Transform>();
-    }
+	}
 
 	void Update()
-    {
-        if(Input.GetKey(KeyCode.W) && ground == true)
-        {
-            transform.Translate(Vector3.up * jump * Time.deltaTime);
-            ground = false;
-        }
+	{
+		if(!ground && transform.position.y >= 0 && transform.position.y <= .01)
+		{
+			ground = true;
+		}
 
-        if(Input.GetKey(KeyCode.A))
-        {
-            transform.position += Vector3.left * speed * Time.deltaTime;
-        }
+		if (Input.GetKey(KeyCode.W) && ground == true)
+		{
+			transform.Translate(Vector3.up * jump * Time.deltaTime);
+			ground = false;
+		}
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += Vector3.right * speed * Time.deltaTime;
-        }
-    }
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+	}
 }
