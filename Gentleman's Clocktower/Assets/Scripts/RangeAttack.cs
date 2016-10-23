e072791bed983282ab41cc804f519e88335d06bf
BrittDibble <dibbl001@cougars.csusm.edu>
2016-10-22 17:46:57 -0700

    Range Attack Bullet Spawn

--- Gentleman's Clocktower/Assets/Scripts/RangeAttack.cs
+++ Gentleman's Clocktower/Assets/Scripts/RangeAttack.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class RangeAttack : MonoBehaviour {
 
 	public GameObject bullet;
+	public GameObject gunEnd;
 	public float disappearDelay;
-	public float zValue;
+	public float firePower;
 	private Transform gun;
 	private float shownTime;
 
@@ -19,17 +20,33 @@ public class RangeAttack : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetMouseButtonDown (1)){//right click
-			gun.transform.localScale = new Vector3 (1f,1f,1f);
-			shownTime = Time.time;
-			Vector3 mousePos = Input.mousePosition;
-			mousePos.z = Camera.main.transform.position.z * -1;//try -1 here if it doesn't work
-			Vector3 rotationObject = Camera.main.WorldToScreenPoint(gun.position);
-			mousePos.x = mousePos.x - rotationObject.x;
-			mousePos.y = mousePos.y - rotationObject.y;
-			float angle = Mathf.Atan2 (mousePos.y, mousePos.x) * Mathf.Rad2Deg - 90f;
-			gun.rotation = Quaternion.Euler (new Vector3 (0f, 0f, angle));
+			showGun();
+			pointGun();
+			fireGun();
 		}
 		if(Time.time - shownTime > disappearDelay)
-			gun.transform.localScale = new Vector3 (0f,0f,0f);
+			gun.transform.localScale = new Vector3 (0f,0f,0f);//hide gun
+	}
+
+	void showGun(){
+		gun.transform.localScale = new Vector3 (1f,1f,1f);
+		shownTime = Time.time;
+	}
+
+	void pointGun(){
+		Vector3 mousePos = Input.mousePosition;
+		Vector3 rotationObject = Camera.main.WorldToScreenPoint(gun.position);
+		mousePos.x = mousePos.x - rotationObject.x;
+		mousePos.y = mousePos.y - rotationObject.y;
+		float angle = Mathf.Atan2 (mousePos.y, mousePos.x) * Mathf.Rad2Deg - 90f;
+		gun.rotation = Quaternion.Euler (new Vector3 (0f, 0f, angle));
+	}
+
+	void fireGun(){
+		GameObject firedBullet = (GameObject)Instantiate (bullet, 
+			           gunEnd.GetComponent<Transform>().position, 
+			           Quaternion.identity);
+		firedBullet.GetComponent<Rigidbody2D>().AddForce(
+			firedBullet.GetComponent<Transform>().forward * firePower);
 	}
 }
