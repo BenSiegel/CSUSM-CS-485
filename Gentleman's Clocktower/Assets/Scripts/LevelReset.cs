f4eccdcb9d70b71a9ce411a1aca2e654a71b7237
BrittDibble <dibbl001@cougars.csusm.edu>
2016-10-24 14:18:24 -0700

    Added Restart Key

---
+++ Gentleman's Clocktower/Assets/Scripts/LevelReset.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelReset : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.R)){
+			int scene = SceneManager.GetActiveScene ().buildIndex;
+			SceneManager.LoadScene (scene, LoadSceneMode.Single);
+		}
+	}
+}
