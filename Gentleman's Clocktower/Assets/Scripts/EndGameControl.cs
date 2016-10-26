9de52e1c9e556890ad9c83747047b54cfb51e1bd
BrittDibble <dibbl001@cougars.csusm.edu>
2016-10-25 17:43:35 -0700

    Added Reset Game when the top of the tower is reached

---
+++ Gentleman's Clocktower/Assets/Scripts/EndGameControl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class EndGameControl : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.tag.Equals("Player")){
+			int scene = SceneManager.GetActiveScene ().buildIndex;
+			SceneManager.LoadScene (scene, LoadSceneMode.Single);
+		}
+	}
+}
