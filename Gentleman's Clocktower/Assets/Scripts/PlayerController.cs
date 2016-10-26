d2c4a854d0c1a681d9f4e93323eba29988440fc3
BrittDibble <dibbl001@cougars.csusm.edu>
2016-10-26 00:32:58 -0700

    Basic Player Damage

--- Gentleman's Clocktower/Assets/Scripts/PlayerController.cs
+++ Gentleman's Clocktower/Assets/Scripts/PlayerController.cs
@@ -6,13 +6,15 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
     public float jump;
+	public int health;
 
     private Transform tm;
     private bool canJump = true;
 
     void Start()
     {
-        tm = GetComponent<Transform>();
+		health = 10;
+		tm = GetComponent<Transform>();
     }
 
     void Update()
@@ -43,5 +45,7 @@ public class PlayerController : MonoBehaviour
     {
         if (col.gameObject.tag.Equals("Ground"))
             canJump = true;
+		if (col.gameObject.tag.Equals ("Enemy"))
+			health--;
     }
 }
