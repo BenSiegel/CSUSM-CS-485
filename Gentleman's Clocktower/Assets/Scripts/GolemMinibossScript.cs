d3db6978411b6117fad08d234d063fd90164c5bc
BrittDibble <dibbl001@cougars.csusm.edu>
2016-11-05 13:47:41 -0700

    GolemStateMachine added

---
+++ Gentleman's Clocktower/Assets/Scripts/GolemMinibossScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GolemMinibossScript : MonoBehaviour {
+
+	public int health;
+
+	enum Actions: int {Track=0, Jump, Attack, SlamUp, SlamDown, Charge};
+	private int currentAction;
+	private GameObject player;
+	private Vector3 moveToPoint;
+	private float actionTime;
+
+	// Use this for initialization
+	void Start () {
+		currentAction = (int)Actions.Track;
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
