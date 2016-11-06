c8b5b3e35560056793df7e0ed6a7cbd01fc0cd8e
BrittDibble <dibbl001@cougars.csusm.edu>
2016-11-06 14:57:57 -0800

    Added Golem Spawn

---
+++ Gentleman's Clocktower/Assets/Scripts/SpawnGolemTriggerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnGolemTriggerScript : MonoBehaviour {
+
+	public GameObject golem;
+
+	private bool spawnedGolem = false;
+
+	void OnTriggerEnter2D(Collider2D collision){
+		if(!spawnedGolem && collision.gameObject.tag.Equals("Player")){
+			spawnedGolem = true;
+			Transform tm = GetComponent<Transform>();
+			Vector3 pos = new Vector3 (tm.position.x, tm.position.y+5);
+			GameObject newGolem = (GameObject)Instantiate (golem, pos, Quaternion.identity);
+		}
+	}
+}
