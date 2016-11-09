adcdcf0b72349016d0f1c079d7bd2d76db8515e3
BrittDibble <dibbl001@cougars.csusm.edu>
2016-11-08 17:19:25 -0800

    Platformer Collsions Working

---
+++ Gentleman's Clocktower/Assets/Scripts/ColliderPlatformScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderPlatformScript : MonoBehaviour {
+
+	private Collider2D setCol;
+
+	void Start(){
+		setCol = GetComponent<Collider2D>();
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (!col.gameObject.tag.Equals ("Ground")) {
+			Physics2D.IgnoreCollision (setCol, col, true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		if (!col.gameObject.tag.Equals ("Ground")) {
+			Physics2D.IgnoreCollision (setCol, col, false);
+		}
+	}
+}
