38f71a34902b43a6092e06b64587033b178840bd
BrittDibble <dibbl001@cougars.csusm.edu>
2016-11-28 13:06:18 -0800

    Level One Finish

---
+++ Gentleman's Clocktower/Assets/Scripts/LoadNextLevelScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevelScript : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D collision){
+		if(collision.gameObject.tag.Equals("Player")){
+			SceneManager.LoadScene("LevelTwoTheTower", LoadSceneMode.Single);
+		}
+	}
+}
