36a7ace50f3c7a50b4aff52c3e7162a6ee9abe49
BrittDibble <dibbl001@cougars.csusm.edu>
2016-12-10 22:28:37 -0800

    Misc File changes to Level2

---
+++ Gentleman's Clocktower/Assets/Scripts/CrushScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrushScript : MonoBehaviour { //example script that is not used on any objects.
+
+	bool wall1 = false;
+	bool wall2 = false;
+
+	void Update(){
+		if( wall1 && wall2 ){
+			//Die();
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.transform.tag == "Crush"){
+			wall1 = true;
+		}
+		else if(other.transform.tag == "Crush1"){
+			wall2 = true;
+		}
+	}
+
+
+	void OnCollisionExit(Collision other)
+	{
+		if (other.transform.tag == "Crush"){
+			wall1 = false;
+		}
+		else if(other.transform.tag == "Crush1"){
+			wall2 = false;
+		}
+	}
+}
