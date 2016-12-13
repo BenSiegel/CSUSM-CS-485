a321543ddcc3870e0e52a215e6b58ab3410c460c
Benjamin Siegel <siege012@cougars.csusm.edu>
2016-12-13 12:37:49 -0800

    Added level 2 endpoint
    ^

---
+++ Gentleman's Clocktower/Assets/Scripts/LoadNextLevelScript3.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevelScript3 : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            SceneManager.LoadScene("LevelThreeFinalBoss", LoadSceneMode.Single);
+        }
+    }
+}
