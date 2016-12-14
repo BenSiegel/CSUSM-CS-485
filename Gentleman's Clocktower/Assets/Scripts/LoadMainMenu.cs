ec49112489cb3472375ee3c96c3fe69fa53e0029
Benjamin Siegel <siege012@cougars.csusm.edu>
2016-12-13 16:42:39 -0800

    End Game
    End Game screen created. Just need to connect it to the final level

---
+++ Gentleman's Clocktower/Assets/Scripts/LoadMainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMainMenu : MonoBehaviour
+{
+    public void onClickButton1()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+}
